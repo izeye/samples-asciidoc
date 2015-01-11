817310b6883ed542d980e9fa53e3dd3f2192e8f8
izeye <izeye@naver.com>
2015-01-11 15:39:05 +0900

    Add a sample from http://vimeo.com/100315341.

---
+++ src/main/java/samples/asciidoc/HelloWorld.java
@@ -0,0 +1,14 @@
+package samples.asciidoc;
+
+/**
+ * Created by izeye on 15. 1. 11..
+ */
+public class HelloWorld {
+
+	// tag::main[]
+	public static void main(String[] args) { // <1>
+		System.out.println("Hello, world!"); // <2>
+	}
+	// end::main[]
+
+}
